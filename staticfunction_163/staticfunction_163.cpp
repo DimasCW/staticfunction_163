a6795fe21d28712fd5dd0931610f625f7239daef
DimasCW <cdimas292@gmail.com>
2023-05-15 21:37:10 +0700

    mengisi class mahasiswa

--- staticfunction_163/staticfunction_163.cpp
+++ staticfunction_163/staticfunction_163.cpp
@@ -4,7 +4,13 @@
 using namespace std;
 
 class mahasiswa {
-
+public:
+	static int nim;
+	int id;
+	string nama;
+	void setid();
+	void printall();
+	mahasiswa(string pnama) :nama(pnama) { setid(); }
 };
 
 
